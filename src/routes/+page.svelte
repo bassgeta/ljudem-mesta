55767eacd62ffbf74eb012f7e851f8c2cdf2a5fd
Bassgeta <kerry.mahne@gmail.com>
2024-01-06 13:48:11 +0100

    feat: i18n

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n'
+</script>
+<svelte:head>
+    <title>{$_('landing.title')}</title> 
+</svelte:head>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
