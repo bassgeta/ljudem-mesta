0ab5ed6b4216ec1dd84efa2930bd87ed052f426b
anze-k <anze@kozak.si>
2024-01-27 19:50:47 +0100

    easy restrictions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,15 @@
 <script>
-	import LocaleSwitcher from '../components/LocaleSwitcher.svelte';
+	import LocaleSwitcher from '../components/LocaleSwitcher.svelte'
+	import CurrentRestrictionsMap from '../components/CurrentRestrictionsMap.svelte'
+
   import '../global.css'
+  import '../../node_modules/mapbox-gl/dist/mapbox-gl.css'
 </script>
 
 <div class="page">
+  <CurrentRestrictionsMap />
   <LocaleSwitcher />
+  
   <slot />
 </div>
 
