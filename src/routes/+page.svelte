46f16deee9d6de35be02ab61f3d32f793b04c80f
Klemen Kogovšek <klemen.kogovsek1@gmail.com>
2024-04-27 16:50:32 +0000

    Remove verification meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,8 +35,6 @@
 	<meta property="twitter:title" content={$_('landing.head.title')} />
 	<meta property="twitter:description" content={$_('landing.head.description')} />
 	<meta property="twitter:image" content="/airbnb-og.jpg" />
-
-	<meta name="google-site-verification" content="qipYjZl25gtrZwmvlQRPRDkbCJyErbpJ4k8OSXuGCqk" />
 	<!-- Meta Tags Generated with https://metatags.io -->
 </svelte:head>
 
