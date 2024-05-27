c873fc1e56ecc02d22067bd4589f8838015a522d
Klemen Kogovsek <klemen.kogovsek1@gmail.com>
2024-05-27 08:58:46 +0200

    Stalisce

--- src/routes/stalisce/+page.svelte
+++ src/routes/stalisce/+page.svelte
@@ -40,6 +40,119 @@
 			<div class="icon"><img src="{hat}" /></div>
 			KAKO LAHKO ZAKON ŠE IZBOLJŠAMO?
 		</h2>
+		<div class="info-card info-card-stalisce">
+			<div class="info-card-body">
+				<h3>
+					Oddaja v kratkotrajni najem naj se zameji na fizične osebe s prijavljenim stalnim
+					prebivališčem na naslovu nepremičnine za kratkotrajni najem.
+				</h3>
+				<p>
+					Predlog zakona v trenutni obliki ne naslavlja problematičnosti naložb za oddajo, ki
+					zajedajo trg dolgotrajnega najema. Kratkoročni najem predstavlja priložnost za velike
+					zaslužke, posledično pa gre velikokrat za trajno gospodarsko aktivnost. Obenem trenutni
+					predlog zakona, ki ne zamejuje kratkotrajnega najema na fizične osebe, ki v stanovanju
+					tudi prebivajo, vzbuja skrb, da bodo najemodajalci v turističnih krajih sklepali najemne
+					pogodbe le za toliko dni v letu oziroma tiste dni v letu, ko ne bodo imeli možnosti
+					oddajati turistom.
+				</p>
+				<p>
+					Zato predlagamo, da lahko v kratkotrajni najem oddajajo samo fizične osebe, ki stalno
+					prebivajo na naslovu nepremičnine za kratkotrajni najem. Posamezniki, ki želijo oddajati
+					za daljše obdobje in/ali nimajo prijavljenega prebivališča na naslovu te nepremičnine, bi
+					se v tem primeru obravnavali kot komercialni ponudniki in bi se morali tudi ustrezno
+					registrirati.
+				</p>
+			</div>
+		</div>
+		<div class="info-card info-card-stalisce">
+			<div class="info-card-body">
+				<h3>
+					Potrebno je omogočiti tako zaostritev pogojev za kratkotrajni najem s strani občine kot
+					tudi povečanje varovalk glede sproščanja pogojev s strani občine.
+				</h3>
+				<p>
+					Omogočanje zaostritve pogojev s strani občine je ključna za nekatere najbolj problematične
+					lokacije v Sloveniji. Z možnostjo zaostritve pogojev bi namreč občini omogočili, da lahko
+					recimo omeji odstotek stanovanj, ki so namenjena kratkoročnemu najemu na določenih
+					lokacijah, ali pa prepove oddajo parkirnega mesta v večstanovanjski stavbi. S tem bi zakon
+					še bolje naslovil raznolike in specifične problematike kratkotrajnega najema.
+				</p>
+				<p>
+					Dodatne varovalke glede sproščanja pogojev so potrebne, da bi zmanjšali vpliv različnih
+					interesnih skupin na župane oziroma občinske svete. Predlagamo, da je analiza
+					sprejemljivih sprememb obvezna za rahljanje pogojev, obenem pa se vanjo kot obvezno
+					vključi tudi oceno učinka na najemni trg v občini. Izvedba takšne analize je ključna za
+					strokovno in objektivno odločitev v primeru podaljšanja časovnega razpona oddaje, ki lahko
+					pomembno vpliva na najemni trg v občini, kot tudi obremenitev lokalne infrastrukture.
+				</p>
+			</div>
+		</div>
+		<div class="info-card info-card-stalisce">
+			<div class="info-card-body">
+				<h3>Za uspešnost zakona je ključno okrepiti izvajanje politike in nadzor.</h3>
+				<p>
+					Omejevanje kratkotrajnega najema stanovanj je lahko dober ukrep samo, če se bo ustrezno
+					izvajal in nadziral.
+				</p>
+
+				<p>Zato je potrebno:</p>
+				<ul>
+					<li>okrepiti delo občinskih inšpekcij s podporo tržnega inšpektorata,</li>
+					<li>vzpostaviti sistem anonimne prijave kršitev,</li>
+					<li>vzpostaviti mehanizem, ki omogoča preverbo opravljanja dejavnosti v stanovanjih,</li>
+					<li>
+						vzpostaviti kazenske določbe za fizične osebe, ki v kratkotrajni najem oddajajo na črno.
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="info-card info-card-stalisce">
+			<div class="info-card-body">
+				<h3>Ponudnike naj se zaveže k objavi identifikacijske številke v oglasih.</h3>
+				<p>
+					Zakon ne ureja obveznosti javne objave identifikacijske številke nastanitvenega prostora
+					na platformah za kratkotrajni najem oziroma v drugih oblikah oglaševanja stanovanj v
+					turistične namene (recimo mali oglasi, družbenomedijske platforme, kot so Facebook,
+					Instagram in podobno). Brez te obveze za ponudnike kratkotrajnega najema bo težko
+					zagotoviti, da se bodo identifikacijske številke dejansko objavljale, s tem pa bo izjemno
+					oteženo izvajanje nadzora.
+				</p>
+				<p>
+					Zato predlagamo, da se ponudnike zaveže, da kratkotrajni najem oglašujejo z obvezno objavo
+					identifikacijske številke. Obenem naj se doda se tudi kazenske določbe za primere, ko
+					ponudniki oglašujejo brez identifikacijske številke.
+				</p>
+			</div>
+		</div>
+		<div class="info-card info-card-stalisce">
+			<div class="info-card-body">
+				<h3>
+					Potrebno je odpraviti nejasnosti glede opredelitve pojmov sobodajalec in ponudnik
+					kratkotrajnega najema.
+				</h3>
+				<p>
+					Zaradi oblice nejasnosti predlagamo, da zakon uvede različna poimenovanja za različne
+					opredelitve in se s tem jasno razloči pravne subjekte od fizičnih oseb, kot tudi pravni
+					status od izvajanja dejavnosti.
+				</p>
+				<p>
+					Zaradi ustaljenosti izraza sobodajalec, ki je že uvedena tudi v drugi zakonodaji,
+					predlagamo, da se izraz sobodajalec ohrani za fizično osebo, ki opravlja dejavnost.
+					Aktivnost izvajanja dejavnosti v stanovanju pa naj bo poimenovano z novo uvedenim izrazom
+					ponudnik kratkotrajnega najema, ki lahko velja tako za fizično osebo kot za pravni
+					subjekt.
+				</p>
+			</div>
+		</div>
+		<div class="info-card info-card-stalisce">
+			<div class="info-card-body">
+				<h3>
+					Natančneje naj se opredelijo vrste nastanitvenih obratov, omejitve v registracijskem
+					postopku, odtegnitev soglasja etažnih lastnikov, obenem pa naj se spremeni število ležišč
+					v večstanovanjski stavbi iz 6 na 4.
+				</h3>
+			</div>
+		</div>
 		<div class="info-card info-card-comment">
 			<div class="info-card-body">
 				<div>
@@ -47,6 +160,11 @@
 					<p>
 						Vsakdo lahko odda komentar na predlog zakona na portalu eDemokracija. Vse, kar moraš
 						storiti je, da skopiraš naš komentar! P.S.: Seveda pa lahko pripraviš tudi svoj predlog.
+					</p>
+					<p>
+						Naj glas zagovornikov stožje regulacije kratkotrajnega najema doseže tudi odločevalce.
+						Vsakdo lahko odda komentar na predlog zakona na portalu eDemokracija. Vse, kar moraš
+						storiti je, da oblikuješ svoj predlog ali pa preprosto skopiraš naš komentar!
 					</p>
 				</div>
 				<div class="stalisce-bubble">
@@ -56,13 +174,19 @@
 		</div>
 
 		<div class="call-to-action">
-			<a class="shadowed-card shadowed-button stalisce-btn dokument" target="_blank">
+			<a
+				class="shadowed-card shadowed-button stalisce-btn dokument"
+				target="_blank"
+				href="https://drive.google.com/file/d/1z7HFbnPKXfNZ0qdeKF3frCM_fj2ZmWEo/view?usp=sharing">
 				<div class="icon">
 					<svelte:component this="{download}" />
 				</div>
 				Prenesi dokument s celotnim komentarjem
 			</a>
-			<a class="shadowed-card shadowed-button stalisce-btn portal" target="_blank">
+			<a
+				class="shadowed-card shadowed-button stalisce-btn portal"
+				target="_blank"
+				href="https://e-uprava.gov.si/storitve/pripraviObrazec.esju?id=2024&dokument=215582">
 				<div class="icon">
 					<svelte:component this="{msg}" />
 				</div>
@@ -238,6 +362,41 @@
 		right: 1rem;
 		@media screen and (max-width: 767px) {
 			top: -3rem;
+		}
+	}
+
+	.info-card-stalisce {
+		&:nth-child(odd) {
+			margin-right: 2rem;
+		}
+		&:nth-child(even) {
+			margin-left: 2rem;
+		}
+		@media screen and (max-width: 767px) {
+			margin: 0;
+			margin-bottom: 1rem;
+			padding: 0;
+			&:nth-child(odd) {
+				margin-right: 0;
+			}
+			&:nth-child(even) {
+				margin-left: 0;
+			}
+		}
+
+		& h3 {
+			font-size: 24px;
+			font-weight: 700;
+			line-height: 28px;
+			margin-bottom: 0.5rem;
+		}
+
+		& p,
+		& ul {
+			font-size: 21px;
+			font-weight: 500;
+			line-height: 28px;
+			margin-bottom: 0.5rem;
 		}
 	}
 </style>
